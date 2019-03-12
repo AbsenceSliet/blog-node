df34e0919c4a7cb4c8d1aea91dbc6ae403862b45
gitwork <1584123890@qq.com>
2019-03-12 23:07:26 +0800

    调通登录注册接口

--- server/controller/admin/admin.js
+++ server/controller/admin/admin.js
@@ -1,9 +1,18 @@
 import AdminModel from '../../modles/admin/admin'
 import BaseComponent from '../../mixins/baseComponent'
-import config from '../../app.config'
+import {
+    Base64
+} from 'js-base64'
+import { AUTH } from '../../app.config'
 import Crypto from 'crypto'
 import jwt from 'jsonwebtoken'
-import dtime from 'time-formater'
+import time from 'time-formater'
+import moment from 'moment'
+import {
+    handleSuccess,
+    handleError
+}
+from '../../utils/helper'
 
 class Admin extends BaseComponent {
     constructor() {
@@ -15,42 +24,47 @@ class Admin extends BaseComponent {
     async login(req, res, next) {
         const { username, password, status = 1 } = req.body
         if (!username) {
-            // throw new Error('用户名参数错误')
+            throw new Error('用户名参数错误')
         } else if (!password) {
-            // throw new Error('密码参数错误')
+            throw new Error('密码参数错误')
         }
-        const newpassword = this.md5Decode(password)
 
+        const newpassword = this.md5Decode(this.decodePassword(password))
 
         const admin = await AdminModel.findOne({ username })
+            // admin.password = admin.password || this.md5Decode(AUTH.defaultPassword)
         if (!admin) {
+
             const adminTip = status == 1 ? `普通会员` : `超级管理员`
             const admin_id = await this.getId('admin_id')
             const adminContent = {
                 username: username,
                 password: newpassword,
-                create_time: dtime().format('YYYY-MM-DD HH:mm'),
+                create_time: moment().format('YYYY-MM-DD HH:mm:ss'),
                 admin: adminTip,
                 status,
                 admin_id: admin_id
             }
             await AdminModel.create(adminContent)
-            console.log(req.session)
                 // req.session.admin_id = admin_id;
-            res.send({
-                status: 1,
-                success: '注册管理员成功',
-            })
-        } else if (newpassword.toString() != admin.password.toString()) {
-            res.send({
-                status: 0,
-                message: `用户名已经存在，密码错误`
+            handleSuccess({ res, message: '注册管理员成功' })
+        } else if (newpassword != admin.password) {
+            handleError({
+                res,
+                message: '用户名已经存在，密码错误'
             })
         } else {
             // req.session.admin_id = admin_id;
-            res.send({
-                status: 1,
-                success: `登录成功`
+            const token = jwt.sign({
+                data: AUTH.data,
+                exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 7)
+            }, AUTH.jwtToken)
+            handleSuccess({
+                res,
+                result: {
+                    token
+                },
+                message: '登陆成功'
             })
         }
     }
--- server/utils/helper.js
+++ server/utils/helper.js
@@ -1,11 +1,15 @@
-export handleSuccess = ({ res, result = null, message = '请求成功' }) => {
+export const handleSuccess = ({ res, result = null, message = '请求成功' }) => {
     res.send({
         code: 1,
         message,
         result
     })
 }
-export handleError = ({ res, err = null, message = '请求失败', }) => {
+export const handleError = ({
+    res,
+    err = null,
+    message = '请求失败',
+}) => {
     res.send({
         code: 0,
         message,

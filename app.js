bd26dbc9bf00cc417a7b9930899a68fee36db66c
gitwork <1584123890@qq.com>
2019-06-11 15:53:08 +0800

    xiugai app.js nginx

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.static(path.resolve(__dirname, './dist')))
 
 app.get('*', function(req, res) {
-    const html = fs.readFileSync(path.resolve(__dirname, './dist'), 'utf-8')
+    const html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8')
     res.send(html)
 })
 app.listen(8082, res => {
